c3c8dd0b7d12b79471be7610e113d84040e2676f
kb4954 <kb4954@naver.com>
2025-04-30 18:25:48 +0900

    오라클 수정

--- py0430/py0430_01.py
+++ py0430/py0430_01.py
@@ -10,7 +10,7 @@ name = input("검색하려는 이름을 입력하세요.")
 
 name = '%'+name+'%'
 
-query = "select emp_name,employee_id, salary from employees where emp_name like '%:name%"
+query = "select emp_name,employee_id, salary from employees where emp_name like:name"
 cursor.execute(query,name=name) # sql구문 F9실행
 rows = cursor.fetchall() # 데이터를 가져옴
 ## employees 월급이 4000~6000 사이의 사번, 이름 월급을 출력하시오
